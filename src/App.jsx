f43d2f24c0506553c7d30ba900a348d77319b2d1
Hercules3246 <mquinter90@gmail.com>
2024-05-15 10:00:49 -0500

    using useMemo  features/add and remove from cart

--- src/App.jsx
+++ src/App.jsx
@@ -4,17 +4,38 @@ import Guitar from "./components/Guitar";
 import { db } from "./data/db";
 function App() {
   const [data, setData] = useState(db);
+  const [cart, setCart] = useState([]);
 
+  function addToCart(item) {
+    const itemExits = cart.findIndex((guitar) => guitar.id === item.id);
+    if (itemExits >= 0) {
+      //hay en el carrito un mismo item entonces aumentar cantidad
+      const updatedCart = [...cart];
+      updatedCart[itemExits].quantity++;
+      setCart(updatedCart);
+    } else {
+      item.quantity = 1;
+      setCart([...cart, item]);
+    }
+  }
+  function removeFromCart(id) {
+    setCart((prevCart) => prevCart.filter((guitar) => guitar.id !== id));
+  }
   return (
     <>
-      <Header />
+      <Header cart={cart} removeFromCart={removeFromCart} />
 
       <main className="container-xl mt-5">
         <h2 className="text-center">Nuestra Colección</h2>
 
         <div className="row mt-5">
           {data.map((guitar) => (
-            <Guitar key={guitar.id} guitar={guitar} />
+            <Guitar
+              key={guitar.id}
+              guitar={guitar}
+              setCart={setCart}
+              addToCart={addToCart}
+            />
           ))}
         </div>
       </main>
